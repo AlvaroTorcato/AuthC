ee1a72047a213a921f13e80cb2bbcb7c6f3a5cd6
Álvaro Torcato <alvaromanuelreis@gmail.com>
2022-11-15 17:42:00 +0000

    Initial commit

---
+++ src/main/java/com/example/authc/AuthCApplication.java
@@ -0,0 +1,13 @@
+package com.example.authc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthCApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthCApplication.class, args);
+    }
+
+}
