efa19967af866ded5a73e24293a03c87243ab69e
Álvaro Torcato <alvaromanuelreis@gmail.com>
2022-12-24 18:43:25 +0000

    Rabbit Com queue e exchange dinamicos

--- src/main/java/com/example/service/RabbitMQConfig.java
+++ src/main/java/com/example/service/RabbitMQConfig.java
@@ -27,14 +27,14 @@ public class RabbitMQConfig {
     public FanoutExchange fanout () {
         return new FanoutExchange("Auth");
     }
-    @Bean
+    /*@Bean
     public Queue autoDeleteQueue() {
         return new AnonymousQueue();
     }
     @Bean
     public Binding binding (FanoutExchange fanout, Queue autoDeleteQUeue){
         return BindingBuilder.bind(autoDeleteQUeue).to(fanout);
-    }
+    }*/
 
 /*
     String products1Queue = "authentications1_queue_fanout";
